bef32ecfccc4d6273b95ac10eb7b2a38eacc0ef1
Onnion <None>
2020-04-23 13:14:30 -0300

    master | add fix conversion

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,11 @@
+const convertion = function () {
+    const value = $(".form__input.currency").val();
+    const price = value.replace('R$ ', '').replace(/\./g, "").replace(',', '.');
+    const hb_value = 44;
+
+    $('.result h1').text((price / hb_value).toFixed(1));
+};
+
 $(document).ready(function () {
     $(".form__input.currency").maskMoney({
         prefix: "R$ ",
@@ -6,12 +14,13 @@ $(document).ready(function () {
     });
 
     $('.form__submit__button').click(function () {
-        const value = $(".form__input.currency").val();
-        const price = value.replace('R$ ', '').replace(/\./g, "").replace(',', '.');
-        const hb_value = 44;
+        convertion();
+    });
 
-        console.log(price);
-        $('.result h1').text((price / hb_value).toFixed(1));
+    $(".form__input.currency").on("keyup", function (event) {
+        if (event.keyCode === 13) {
+            convertion();
+        }
     });
 
 });
